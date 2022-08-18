d52115a6f1d07479159512caf44433349c3173e5
andersonlima013 <andersonwow302@gmail.com>
2022-08-18 13:24:33 -0300

    comentario adicional criaCliente

--- projeto_inicial/admin/controller/criaClientes-controller.js
+++ projeto_inicial/admin/controller/criaClientes-controller.js
@@ -4,6 +4,11 @@ import { clienteService } from "../service/cliente-service.js"
 // percorrendo a arvore do DOM para encontrar o nosso formulario, onde vamos criar uma nova linha de cliente
 const formulario = document.querySelector('[data-form]')
 
+/* 
+usamos aqui o formulario que encontramos na variavel acima para poder adicionar um eventListener aqui, assim podemos aguardar o evento de 
+'submit' e com o evento ocorrendo, criamos duas constantes temporárias de nome e email que serão passadas como argumento lá na função 
+que veio exportada de clienteService, criando assim um novo cliente
+*/
 formulario.addEventListener('submit', (evento) => {
     evento.preventDefault()
     const nome = evento.target.querySelector('[data-nome]').value
